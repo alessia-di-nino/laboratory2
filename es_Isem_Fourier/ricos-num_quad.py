2bd57257b6157c9d4e526dca094acd59d13b26d0
alessia.dinino <alessia.dinino@outlook.it>
2023-12-30 12:06:34 +0100

    modifiche

--- es_Isem_Fourier/ricos-num_quad.py
+++ es_Isem_Fourier/ricos-num_quad.py
@@ -18,21 +18,31 @@ frequency = 10.0  # Frequenza della forma d'onda in Hertz
 amplitude = 1.0  # Ampiezza della forma d'onda
 
 # Parametri della serie di Fourier
-num_terms = 1000  # Numero di termini nella serie di Fourier
+num_terms1 = 1  # Numero di termini nella serie di Fourier
+num_terms2 = 5
 
 # Creazione di un array di valori temporali
 t = np.linspace(0, 2, 1000, endpoint=False)
 
 # Calcolo della forma d'onda quadra e della sua approssimazione tramite serie di Fourier
 
-fourier_approximation = fourier_series_square(t, frequency, amplitude, num_terms)
+fourier_approximation1 = fourier_series_square(t, frequency, amplitude, num_terms1)
+fourier_approximation2 = fourier_series_square(t, frequency, amplitude, num_terms2)
 
+'''
 # Plot dei risultati
 plt.figure(figsize=(10, 6))
 plt.plot(t, fourier_approximation, label=f'Approssimazione con {num_terms} termini')
 plt.title('Ricostruzione numerica di una forma d\'onda quadra tramite serie di Fourier')
-plt.xlabel('Tempo (s)')
-plt.ylabel('Ampiezza')
+plt.xlabel('Tempo [T]')
+plt.ylabel('Segnale simulato [u.a.]')
 plt.legend()
 plt.grid(True)
+plt.show()
+'''
+
+fig, axs = plt.subplots(2)
+axs[0].plot(t, fourier_approximation1)
+axs[1].plot(t, fourier_approximation2)
+
 plt.show()
--- es_Isem_Fourier/ricos-num_triang.py
+++ es_Isem_Fourier/ricos-num_triang.py
@@ -1,38 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def triangular_waveform(t, frequency, amplitude):
-    return amplitude * (2 / np.pi) * np.arcsin(np.sin(2 * np.pi * frequency * t) / np.pi)
 
-def fourier_series_triangular(t, frequency, amplitude, num_terms):
-    result = np.zeros_like(t)
-    for n in range(1, num_terms + 1):
-        harmonic = 2 * n - 1
-        term = triangular_waveform(t, harmonic * frequency, amplitude) / harmonic
-        result += term
+def fourier_series_triang(t, frequency, amplitude, num_terms):
+    result = []
+    for current_t in t:
+        term = 0
+        for k in range(1, num_terms + 1):
+            bk = 0 if k%2 == 0 else (2/(k * np.pi))**2 
+            term += bk * np.cos(k * frequency * current_t)
+        result.append(term)
+        
     return result
 
-# Parametri della forma d'onda triangolare
-frequency = 1.0  # Frequenza della forma d'onda in Hertz
+# Parametri della forma d'onda quadra
+frequency = 10.0  # Frequenza della forma d'onda in Hertz
 amplitude = 1.0  # Ampiezza della forma d'onda
 
 # Parametri della serie di Fourier
-num_terms = 100000  # Numero di termini nella serie di Fourier
+num_terms = 3  # Numero di termini nella serie di Fourier
 
 # Creazione di un array di valori temporali
 t = np.linspace(0, 2, 1000, endpoint=False)
 
-# Calcolo della forma d'onda triangolare e della sua approssimazione tramite serie di Fourier
-triangular_wave = triangular_waveform(t, frequency, amplitude)
-fourier_approximation = fourier_series_triangular(t, frequency, amplitude, num_terms)
+# Calcolo della forma d'onda quadra e della sua approssimazione tramite serie di Fourier
+
+fourier_approximation = fourier_series_triang(t, frequency, amplitude, num_terms)
 
 # Plot dei risultati
 plt.figure(figsize=(10, 6))
-plt.plot(t, triangular_wave, label='Forma d\'onda triangolare')
 plt.plot(t, fourier_approximation, label=f'Approssimazione con {num_terms} termini')
 plt.title('Ricostruzione numerica di una forma d\'onda triangolare tramite serie di Fourier')
-plt.xlabel('Tempo (s)')
-plt.ylabel('Ampiezza')
+plt.xlabel('Tempo [T]')
+plt.ylabel('Segnale simulato [u.a.]')
 plt.legend()
 plt.grid(True)
 plt.show()
